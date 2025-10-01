79ed7e658dd9f32e6e0796e3c131abd8d0b47e05
gabriel <gabriel_jiglau@yahoo.com>
2025-10-01 21:58:56 +0300

    start training loop

--- src/bnp_gmm.py
+++ src/bnp_gmm.py
@@ -3,16 +3,16 @@ import pandas as pd
 import pickle
 from dataclasses import dataclass
 from scipy.stats import gamma, beta
-from scipy.special import psi
+from scipy.special import psi, logsumexp
 from utils import *
 
 @dataclass
-class NiwPriors:
+class PriorHyperparameters:
 
     """
-    priors for the Normal Inverse Wishart distribution(data mean, belief of strength in the mean, degrees of freedom
-                                                       and the scale matrix)
+    priors: data mean, belief of strength in the mean, degrees of freedom and the scale matrix
     """
+
     miu_0: np.ndarray  # data mean
     beta_0: int  # belief of strength in the mean
     niu_0: int  # degrees of freedom
@@ -20,12 +20,22 @@ class NiwPriors:
 
 
 @dataclass
-class StickPriors:
-    a_0: int
-    b_0: int
+class MixingWeight:
+
+    """
+    each weight ~ Beta(a_k, b_k)
+    """
+
+    a_k: int
+    b_k: int
+    weight: float
 
 
-class GammaDistribution:
+class VariationalGamma:
+
+    """
+    prior for alpha (the concentration parameter) ~ Gamma(a, b)
+    """
 
     def __init__(self, a: int=1, b: int=1):
         self.a = a
@@ -75,27 +85,95 @@ class BayesianNonparametricMixture:
     approximation of the Dirichlet Process (DP) for GMMs, using a truncated Stick-Breaking prior
     """
 
-    def __init__(self, alpha: GammaDistribution, h: NiwPriors, truncated_clusters=50):
+    def __init__(self, alpha: VariationalGamma=None, truncated_clusters=50):
         """
         :param alpha: concentration parameter that has a prior, in order to be inferred from data
                       (small alpha -few clusters with larger weights-, big alpha -more clusters with smaller weights-)
-        :param h: the base distribution (the prior for cluster parameters)
         """
 
         if alpha is not None:
             self.alpha = alpha
         else:
-            self.alpha = GammaDistribution()
+            self.alpha = VariationalGamma()
 
-        self.h = h
         self.k = truncated_clusters
         self.responsibilities = None
-        self.a_sticks = [object.__new__(StickPriors) for _ in range(self.k)]
+        self.h = object.__new__(PriorHyperparameters) # the base distribution (the prior for cluster parameters)
+        self.posteriors = [object.__new__(NiwPosteriors) for _ in range(self.k)]
+        self.sticks = [object.__new__(MixingWeight) for _ in range(self.k)]
 
-    def train(self, num_iterations: int, x_train: pd.DataFrame, y_train=None, posteriors='../models/posteriors.pkl'):
 
+    def train(self, num_iterations: int, x_train: pd.DataFrame, y_train=None, a_k=1, b_k=1,
+              posteriors='../models/posteriors.pkl'):
+
+        # the assumption here is that param x_train is cleaned and standardized
         dim_data = x_train.shape[1]
-        self.responsibilities = np.zeros((k, dim_data))
+        self.responsibilities = np.zeros((self.k, dim_data))
+
+        cluster_means, labels = init_clusters(self.k, x_train)  # cheap KMeans for a decent initialization
+        weights, beta_samples = stick_breaking_prior(a=1, b=1, truncated_clusters=self.k)
+
+        h_priors = init_priors(no_clusters=self.k, x_train=x_train, dim_data=dim_data, epsilon=1e-6)
+        self.h = PriorHyperparameters(h_priors[1], h_priors[2], h_priors[3], h_priors[4])
+
+        # initializations for prior and posteriors
+        for i in range(self.k):
+            self.posteriors[i].miu = cluster_means[i]
+            self.posteriors[i].beta = self.h.beta_0
+            self.posteriors[i].niu = self.h.niu_0
+            self.posteriors[i].p_lambda = self.h.lambda_0
+            self.sticks[i].a_k = a_k
+            self.sticks[i].b_k = b_k
+            self.sticks[i].weight = weights[i]
+
+        for iteration in range(num_iterations):
+
+            self.variational_e_step(x_train, dim_data)
+            self.variational_m_step(x_train, dim_data)
+            
+            
+            
+    
+    def variational_e_step(self, x_train, dim_data):
+        
+        """
+        update the responsibilities (assignment probabilities) for each cluster
+        """
+
+        expectation_component = 0
+        for cluster_idx in range(1, self.k - 1):
+            a_k = self.sticks[cluster_idx].a_k
+            b_k = self.sticks[cluster_idx].b_k
+            expectation_component += psi(b_k) + psi(a_k + b_k)
+
+        log_responsibilities = np.zeros((self.k, x_train.shape[0]))
+        for cluster_idx in range(self.k):
+
+            a_k = self.sticks[cluster_idx].a_k
+            b_k = self.sticks[cluster_idx].b_k
+            log_pi_expectation = psi(a_k) - psi(a_k + b_k) + expectation_component
+            for idx, x_row in enumerate(x_train.itertuples()):
+
+                nominator = (self.posteriors[cluster_idx].beta + 1) * self.posteriors[cluster_idx].p_lambda
+                denominator = self.posteriors[cluster_idx].niu - dim_data + 1
+                denominator *= self.posteriors[cluster_idx].beta
+                scale_matrix = nominator / denominator
+
+                log_pdf = student_t_pdf(np.array(x_row[1:]), self.posteriors[cluster_idx].niu, dim_data,
+                                        self.posteriors[cluster_idx].miu, scale_matrix)
+                log_responsibilities[cluster_idx, idx] = log_pi_expectation + np.log(log_pdf)
+
+        log_responsibilities -= logsumexp(log_responsibilities, axis=0, keepdims=True)
+        self.responsibilities = np.exp(log_responsibilities)
+    
+    def variational_m_step(self, x_train, dim_data):
+        
+        """
+        update the parameters for the concentration parameter, niw and the sticks
+        """
+        
+        pass
+    
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
+from scipy.special import gammaln
 from scipy.stats import beta
+from sklearn.cluster import KMeans
 
 
 def stick_breaking_prior(a: float, b: float, truncated_clusters: int):
@@ -16,8 +18,50 @@ def stick_breaking_prior(a: float, b: float, truncated_clusters: int):
     weights[-1] += remaining_stick  # add what's left to the last cluster
     return weights, beta_samples
 
+def _data_mean(x_train):
 
-## aici sa rulezi un k-means (il folosesti la initializarea clusterelor)
+    data_mean = np.zeros(x_train.shape[1])
+    for i in range(x_train.shape[1]):
+        dimension_mean = x_train.iloc[:, i].mean()
+        data_mean[i] = dimension_mean
+    return data_mean
+
+def init_priors(no_clusters, x_train, dim_data, epsilon):
+
+    """
+    return priors: prior on weights, data_mean, strength_mean (the confidence in the data_mean),
+    degree_of_freedom, scale_matrix
+    """
+
+    return [np.ones(no_clusters),
+            _data_mean(x_train),
+            1,
+            x_train.shape[1] + 1,
+            (np.eye(dim_data) * epsilon) + np.eye(dim_data)]
+
+def init_clusters(k, x_train):
+
+    kmeans = KMeans(n_clusters=k, n_init=3, max_iter=1, random_state=13)
+    kmeans.fit(x_train)
+
+    return kmeans.cluster_centers_, kmeans.labels_
+
+def student_t_pdf(x_in, degrees_of_freedom, dim_data, cluster_mean, scale_matrix):
+
+    scale_matrix = (scale_matrix * scale_matrix.transpose()) / 2
+
+    nominator = gammaln((degrees_of_freedom + dim_data) / 2)
+    denominator = gammaln(degrees_of_freedom / 2) * ((degrees_of_freedom * np.pi) ** dim_data / 2)
+    # print(f"scale_matrix = {scale_matrix}")
+    # print(f"np.linalg.det(scale_matrix) = {np.linalg.det(scale_matrix)}")
+    denominator *= np.sqrt(np.linalg.det(scale_matrix))
+
+    diff = (x_in - cluster_mean).reshape(-1, 1)
+    free_term = (1 + (1 / degrees_of_freedom) * (diff.transpose() @ np.linalg.inv(scale_matrix) @ diff))
+    # print(f"degrees_of_freedom = {degrees_of_freedom}")
+    free_term **= ((degrees_of_freedom + dim_data) / -2)
+
+    return (nominator / denominator) * free_term
 
 if __name__ == '__main__':
 
