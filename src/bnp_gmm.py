8159bcc0c8088eddd72e32707bf15ef0b18333ea
gabriel <gabriel_jiglau@yahoo.com>
2025-10-02 16:12:02 +0300

    start M-step

--- src/bnp_gmm.py
+++ src/bnp_gmm.py
@@ -40,7 +40,7 @@ class VariationalGamma:
     def __init__(self, a: int=1, b: int=1):
         self.a = a
         self.b = b
-        self.p_alpha = 1
+        self.value = 1
 
     def update_posterior(self, k, variational_a, variational_b):
 
@@ -65,17 +65,22 @@ class NiwPosteriors:
         self.niu = niu
         self.p_lambda = p_lambda
 
-    def update_beta(self, beta_0, soft_counts):
-        self.beta = beta_0 + soft_counts
+    def update_beta(self, beta_0, k, soft_counts):
+        self.beta = beta_0 + soft_counts[k]
 
-    def update_niu(self, niu_0, soft_counts):
-        self.niu = niu_0 + soft_counts
+    def update_niu(self, niu_0, k, soft_counts):
+        self.niu = niu_0 + soft_counts[k]
 
     def update_miu(self, beta_0, miu_0, k, soft_counts, weighted_means):
         self.miu = beta_0 * miu_0 + soft_counts[k] * weighted_means[k, :]
         self.miu /= self.beta
 
-    def update_lambda(self, lambda_0, observed_data, uncertainty_coefficient):
+    def update_lambda(self, beta_0, miu_0, lambda_0, k, x_train, soft_counts, weighted_means, responsibilities):
+
+        covariance_matrix = build_sample_covariance(x_train, k, soft_counts, weighted_means, responsibilities)
+        observed_data = soft_counts[k] * covariance_matrix
+        uncertainty_coefficient = build_coefficient(beta_0, miu_0, soft_counts[k], weighted_means[k, :])
+
         self.p_lambda = lambda_0 + observed_data + uncertainty_coefficient
 
 
@@ -96,9 +101,9 @@ class BayesianNonparametricMixture:
         else:
             self.alpha = VariationalGamma()
 
-        self.k = truncated_clusters
+        self.k = truncated_clusters  # up to k clusters maximum
         self.responsibilities = None
-        self.h = object.__new__(PriorHyperparameters) # the base distribution (the prior for cluster parameters)
+        self.priors = object.__new__(PriorHyperparameters) # the base distribution (the prior for cluster parameters)
         self.posteriors = [object.__new__(NiwPosteriors) for _ in range(self.k)]
         self.sticks = [object.__new__(MixingWeight) for _ in range(self.k)]
 
@@ -107,6 +112,8 @@ class BayesianNonparametricMixture:
               posteriors='../models/posteriors.pkl'):
 
         # the assumption here is that param x_train is cleaned and standardized
+
+        x_train = x_train.to_numpy()
         dim_data = x_train.shape[1]
         self.responsibilities = np.zeros((self.k, dim_data))
 
@@ -114,14 +121,14 @@ class BayesianNonparametricMixture:
         weights, beta_samples = stick_breaking_prior(a=1, b=1, truncated_clusters=self.k)
 
         h_priors = init_priors(no_clusters=self.k, x_train=x_train, dim_data=dim_data, epsilon=1e-6)
-        self.h = PriorHyperparameters(h_priors[1], h_priors[2], h_priors[3], h_priors[4])
+        self.priors = PriorHyperparameters(h_priors[1], h_priors[2], h_priors[3], h_priors[4])
 
         # initializations for prior and posteriors
         for i in range(self.k):
             self.posteriors[i].miu = cluster_means[i]
-            self.posteriors[i].beta = self.h.beta_0
-            self.posteriors[i].niu = self.h.niu_0
-            self.posteriors[i].p_lambda = self.h.lambda_0
+            self.posteriors[i].beta = self.priors.beta_0
+            self.posteriors[i].niu = self.priors.niu_0
+            self.posteriors[i].p_lambda = self.priors.lambda_0
             self.sticks[i].a_k = a_k
             self.sticks[i].b_k = b_k
             self.sticks[i].weight = weights[i]
@@ -140,19 +147,19 @@ class BayesianNonparametricMixture:
         update the responsibilities (assignment probabilities) for each cluster
         """
 
-        expectation_component = 0
-        for cluster_idx in range(1, self.k - 1):
-            a_k = self.sticks[cluster_idx].a_k
-            b_k = self.sticks[cluster_idx].b_k
-            expectation_component += psi(b_k) + psi(a_k + b_k)
-
         log_responsibilities = np.zeros((self.k, x_train.shape[0]))
         for cluster_idx in range(self.k):
+
+            expectation_component = 0
+            for j in range(cluster_idx):
+                a_j = self.sticks[j].a_k
+                b_j = self.sticks[j].b_k
+                expectation_component += psi(b_j) - psi(a_j + b_j)
 
             a_k = self.sticks[cluster_idx].a_k
             b_k = self.sticks[cluster_idx].b_k
             log_pi_expectation = psi(a_k) - psi(a_k + b_k) + expectation_component
-            for idx, x_row in enumerate(x_train.itertuples()):
+            for idx, x_row in enumerate(x_train):
 
                 nominator = (self.posteriors[cluster_idx].beta + 1) * self.posteriors[cluster_idx].p_lambda
                 denominator = self.posteriors[cluster_idx].niu - dim_data + 1
@@ -165,15 +172,37 @@ class BayesianNonparametricMixture:
 
         log_responsibilities -= logsumexp(log_responsibilities, axis=0, keepdims=True)
         self.responsibilities = np.exp(log_responsibilities)
-    
+
     def variational_m_step(self, x_train, dim_data):
         
         """
-        update the parameters for the concentration parameter, niw and the sticks
+        update the posteriors for the concentration parameter, niw and the sticks
         """
-        
-        pass
-    
+
+        soft_counts = np.zeros(self.k)
+        for k in range(self.k):
+            soft_counts[k] = np.sum(self.responsibilities[k, :])
+
+        # stick breaking weights update
+        for k in range(self.k):
+            self.alpha.a = 1 + soft_counts[k]
+            self.alpha.b = E[q(alpha)] + sum(i) sum(j > k) resp(i, j)
+
+        # new weighted mean
+        weighted_means = np.zeros((self.k, x_train.shape[1]))
+        for k in range(self.k):
+            for idx, row in enumerate(x_train):
+                weighted_means[k, :] += self.responsibilities[k, idx] * np.array(row[1:])
+            weighted_means[k, :] /= soft_counts[k]
+
+        # NIW posterior parameter updates
+        for k in range(self.k):
+
+            self.posteriors[k].update_beta(self.priors.beta_0, k, soft_counts)
+            self.posteriors[k].update_niu(self.priors.niu_0, k, soft_counts)
+            self.posteriors[k].update_miu(self.priors.beta_0, self.priors.miu_0, k, soft_counts, weighted_means)
+            self.posteriors[k].update_lambda(self.priors.beta_0, self.priors.miu_0, self.priors.lambda_0,
+                                             k, x_train, soft_counts, weighted_means, self.responsibilities)
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -18,6 +18,7 @@ def stick_breaking_prior(a: float, b: float, truncated_clusters: int):
     weights[-1] += remaining_stick  # add what's left to the last cluster
     return weights, beta_samples
 
+
 def _data_mean(x_train):
 
     data_mean = np.zeros(x_train.shape[1])
@@ -25,6 +26,7 @@ def _data_mean(x_train):
         dimension_mean = x_train.iloc[:, i].mean()
         data_mean[i] = dimension_mean
     return data_mean
+
 
 def init_priors(no_clusters, x_train, dim_data, epsilon):
 
@@ -39,12 +41,14 @@ def init_priors(no_clusters, x_train, dim_data, epsilon):
             x_train.shape[1] + 1,
             (np.eye(dim_data) * epsilon) + np.eye(dim_data)]
 
+
 def init_clusters(k, x_train):
 
     kmeans = KMeans(n_clusters=k, n_init=3, max_iter=1, random_state=13)
     kmeans.fit(x_train)
 
     return kmeans.cluster_centers_, kmeans.labels_
+
 
 def student_t_pdf(x_in, degrees_of_freedom, dim_data, cluster_mean, scale_matrix):
 
@@ -62,6 +66,31 @@ def student_t_pdf(x_in, degrees_of_freedom, dim_data, cluster_mean, scale_matrix
     free_term **= ((degrees_of_freedom + dim_data) / -2)
 
     return (nominator / denominator) * free_term
+
+
+def build_sample_covariance(x_train, no_clusters, soft_counts, weighted_means, responsibilities):
+    covariance = []
+
+    for k in range(no_clusters):
+        cov_dim = 0
+        for idx, row in enumerate(x_train):
+            diff = np.array(row[1:]) - weighted_means[k]
+            diff = diff.reshape(-1, 1)
+            cov_dim += (diff @ diff.transpose()) * responsibilities[k, idx]
+
+        # print(f"cov_dim = {cov_dim}")
+        # print(f"soft_counts[k] = {soft_counts[k]}")  # should sum up to 1
+        covariance.append(cov_dim / soft_counts[k])
+
+    return np.array(covariance)
+
+
+def build_coefficient(beta_0, miu_0, soft_count, weighted_mean):
+    first_term = (beta_0 * soft_count) / (beta_0 + soft_count)
+    diff = (weighted_mean - miu_0).reshape(-1, 1)
+
+    return first_term * (diff @ diff.transpose())
+
 
 if __name__ == '__main__':
 
